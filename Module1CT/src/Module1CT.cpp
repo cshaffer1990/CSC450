e60ac3e52cd71d55f5e36e9950149fa0733af3f2
Cameron Shaffer <cshaffer1990@gmail.com>
2025-07-18 16:54:03 -0500

    Readding Module1CT properly

---
+++ Module1CT/src/Module1CT.cpp
@@ -0,0 +1,18 @@
+//Pseudocode:
+//Print first name
+//Print last name
+//Print address
+//Print city
+//Print zip code
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "First Name: Cameron\n";
+    cout << "Last Name: Shaffer\n";
+    cout << "Street Address: 98765 Wallaby Ln.\n";
+    cout << "City: Houston\n";
+    cout << "Zip Code: 77494\n";
+    return 0;
+}
