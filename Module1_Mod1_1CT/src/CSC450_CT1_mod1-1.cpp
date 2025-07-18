7a488668c803359581c17997133a58c33f61244b
Cameron Shaffer <cshaffer1990@gmail.com>
2025-07-18 16:42:01 -0500

    Initial commit with all 3 assignments

---
+++ Module1_Mod1_1CT/src/CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,13 @@
+//Pseudocode:
+//Print Welcome to this C++ Program
+//Print I have corrected all errors for this program.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    return 0;
+}
